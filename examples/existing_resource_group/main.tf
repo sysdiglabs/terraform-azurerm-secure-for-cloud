2f0c0f680f32094c05938d1c6df271b4f20e2b69
regiluze <ruben.eguiluz@sysdig.com>
2021-09-15 16:56:38 +0200

    chore: Remove outdated examples

--- examples/existing_resource_group/main.tf
+++
@@ -1,7 +0,0 @@
-
-module "secure_for_cloud" {
-  source = "sysdiglabs/secure-for-cloud/azurerm"
-
-  sysdig_secure_api_token = var.sysdig_secure_api_token
-  resource_group_name     = var.resource_group_name
-}
