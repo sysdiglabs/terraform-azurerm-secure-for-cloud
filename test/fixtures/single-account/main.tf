ab0f82feaab8a7c67446266889a87815832837c3
regiluze <ruben.eguiluz@sysdig.com>
2021-10-08 00:44:33 +0200

    Feat add kitchen test (#24)
    * feat: Add kitchen test
    
    * feat: add ruby tool and workflow file
    
    * fix: Change the azure login step
    
    * fix: Change the root folder name
    
    * fix: Change azure authentication method
    
    * fix: Remove the azure login step

---
+++ test/fixtures/single-account/main.tf
@@ -0,0 +1,6 @@
+module "cloudvision_example_existing_resource_group" {
+  source = "../../../examples/single-account"
+
+  subscription_id         = var.subscription_id
+  sysdig_secure_api_token = var.sysdig_secure_api_token
+}
