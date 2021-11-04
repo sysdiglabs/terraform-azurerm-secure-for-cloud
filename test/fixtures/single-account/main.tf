1b71956043d8af63ef12b4d09d7bbaacca0c0caf
hayk99 <hayk.kocharyan@sysdig.com>
2021-11-04 16:56:21 +0100

    fix: use another name

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -3,5 +3,5 @@ module "cloudvision_example_existing_resource_group" {
 
   subscription_id         = var.subscription_id
   sysdig_secure_api_token = var.sysdig_secure_api_token
-  name                    = "kitchentest"
+  name                    = "hayk"
 }
