9c2f1714f55388ea736817f77a54d0b667a59ab6
hayk99 <hayk.kocharyan@sysdig.com>
2021-11-08 17:40:30 +0100

    ci: add k8s test in integration tests

--- test/fixtures/single-account-k8s/main.tf
+++ test/fixtures/single-account-k8s/main.tf
@@ -1,5 +1,5 @@
-module "s4c_single_account_example" {
-  source = "../../../examples/single-account"
+module "s4c_single_account_k8s_example" {
+  source = "../../../examples/single-account-k8s"
 
   subscription_id         = var.subscription_id
   sysdig_secure_api_token = var.sysdig_secure_api_token
