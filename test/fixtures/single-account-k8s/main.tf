6ecd9fe530df7fd6c8ae6fa0b70361061d111b8b
Hayk Kocharyan <haykk1999@gmail.com>
2021-11-10 10:56:45 +0100

    ci: add single-account over k8s integration test (#31)
    Co-authored-by: Fede Barcelona <fede.barcelona@sysdig.com>

--- test/fixtures/single-account-k8s/main.tf
+++ test/fixtures/single-account-k8s/main.tf
@@ -1,8 +1,7 @@
-module "s4c_single_account_example" {
-  source = "../../../examples/single-account"
+module "s4c_single_account_k8s_example" {
+  source = "../../../examples/single-account-k8s"
 
   subscription_id         = var.subscription_id
   sysdig_secure_api_token = var.sysdig_secure_api_token
   name                    = "kitchen"
-  deploy_bench            = false
 }
