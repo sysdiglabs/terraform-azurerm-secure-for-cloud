96099d487b9de48bf3196bb59e53ec0d0ed4847c
regiluze <ruben.eguiluz@sysdig.com>
2021-11-10 23:52:28 +0100

    feat: update the single account example to add scanning feature

--- examples/single-account/main.tf
+++ examples/single-account/main.tf
@@ -20,36 +20,47 @@ module "infrastructure_eventhub" {
   resource_group_name = var.resource_group_name
 }
 
+module "infrastructure_eventgrid_eventhub" {
+  source = "../../modules/infrastructure/eventhub"
+
+  subscription_id           = data.azurerm_subscription.current.subscription_id
+  location                  = var.location
+  name                      = "${var.name}eventgrid"
+  resource_group_name       = module.infrastructure_eventhub.resource_group_name
+  deploy_diagnostic_setting = false
+}
+
 module "infrastructure_container_registry" {
   source = "../../modules/infrastructure/container_registry"
 
   location             = var.location
   name                 = var.name
   resource_group_name  = module.infrastructure_eventhub.resource_group_name
-  eventhub_endpoint_id = module.infrastructure_eventhub.azure_eventhub_id
+  eventhub_endpoint_id = module.infrastructure_eventgrid_eventhub.azure_eventhub_id
 }
 
 module "infrastructure_enterprise_app" {
   source = "../../modules/infrastructure/enterprise_app"
 
-  name                = var.name
-  resource_group_name = module.infrastructure_eventhub.resource_group_name
-  subscription_id     = data.azurerm_subscription.current.subscription_id
+  name            = var.name
+  subscription_id = data.azurerm_subscription.current.subscription_id
 }
 
 module "cloud_connector" {
   source = "../../modules/services/cloud-connector"
   name   = "${var.name}-cloudconnector"
 
-  subscription_id                  = data.azurerm_subscription.current.subscription_id
-  resource_group_name              = module.infrastructure_eventhub.resource_group_name
-  azure_eventhub_connection_string = module.infrastructure_eventhub.azure_eventhub_connection_string
-  tenant_id                        = module.infrastructure_enterprise_app.tenant_id
-  client_id                        = module.infrastructure_enterprise_app.client_id
-  client_secret                    = module.infrastructure_enterprise_app.client_secret
-  location                         = var.location
-  sysdig_secure_api_token          = var.sysdig_secure_api_token
-  sysdig_secure_endpoint           = var.sysdig_secure_endpoint
-  verify_ssl                       = local.verify_ssl
-  tags                             = var.tags
+  subscription_id                            = data.azurerm_subscription.current.subscription_id
+  resource_group_name                        = module.infrastructure_eventhub.resource_group_name
+  container_registry                         = module.infrastructure_container_registry.container_registry
+  azure_eventhub_connection_string           = module.infrastructure_eventhub.azure_eventhub_connection_string
+  azure_eventgrid_eventhub_connection_string = module.infrastructure_eventgrid_eventhub.azure_eventhub_connection_string
+  tenant_id                                  = module.infrastructure_enterprise_app.tenant_id
+  client_id                                  = module.infrastructure_enterprise_app.client_id
+  client_secret                              = module.infrastructure_enterprise_app.client_secret
+  location                                   = var.location
+  sysdig_secure_api_token                    = var.sysdig_secure_api_token
+  sysdig_secure_endpoint                     = var.sysdig_secure_endpoint
+  verify_ssl                                 = local.verify_ssl
+  tags                                       = var.tags
 }
