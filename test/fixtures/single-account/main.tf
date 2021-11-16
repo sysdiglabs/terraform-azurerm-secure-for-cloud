bd554dbbcf7a2a7dce1d0e5c20ba9cc320007742
hayk99 <hayk.kocharyan@sysdig.com>
2021-11-16 09:22:12 +0100

    fix: rolling back

--- test/fixtures/single-account/main.tf
+++ test/fixtures/single-account/main.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 module "s4c_single_account_example" {
--- test/fixtures/single-account/variables.tf
+++ test/fixtures/single-account/variables.tf
@@ -3,12 +3,6 @@ variable "subscription_id" {
   description = "The Azure subscription ID to use to deploy the resources"
 }
 
-variable "tenant_id" {
-  type        = string
-  description = "The Azure tenant ID to use to deploy the resources"
-}
-
-
 variable "sysdig_secure_api_token" {
   type        = string
   description = "Sysdig's Secure API Token"
