617262be3503bec4defad015c6670b766ff1afb5
hayk99 <hayk.kocharyan@sysdig.com>
2022-01-25 13:21:32 +0100

    chore: remove unused variable

--- examples/trigger-events/variables.tf
+++ examples/trigger-events/variables.tf
@@ -1,8 +1,3 @@
-variable "subscription_id" {
-  type        = string
-  description = "The Azure subscription ID to use to deploy the resources"
-}
-
 variable "resource_group_name" {
   type        = string
   description = "The resource group name to deploy cloud vision stack"
