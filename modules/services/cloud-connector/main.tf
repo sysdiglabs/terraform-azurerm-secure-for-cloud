973cc7c9ee5317ccc4ef836549af473be246bab3
iru <irune.prado@sysdig.com>
2022-03-31 12:21:14 +0200

    fix: upgraded azure provider

--- modules/services/cloud-connector/main.tf
+++ modules/services/cloud-connector/main.tf
@@ -143,7 +143,7 @@ resource "azurerm_container_group" "cg" {
   name                = "${var.name}-group"
   location            = var.location
   resource_group_name = var.resource_group_name
-  ip_address_type     = "private"
+  ip_address_type     = "Private"
   os_type             = "Linux"
   network_profile_id  = azurerm_network_profile.np.id
 
