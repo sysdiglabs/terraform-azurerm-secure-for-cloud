1bc7ba2130f1ae81d5f96e15caba9804824baf2c
hayk99 <hayk.kocharyan@sysdig.com>
2022-05-20 12:44:40 +0200

    chore: extract provider

--- test/fixtures/single-subscription-k8s/main.tf
+++ test/fixtures/single-subscription-k8s/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 0.15.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.2"
+    }
+    sysdig  = {
+      source  = "sysdiglabs/sysdig"
+      version = ">= 0.5.27"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
@@ -7,6 +21,11 @@ provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
   }
+}
+
+provider "sysdig" {
+  sysdig_secure_url       = var.sysdig_secure_endpoint
+  sysdig_secure_api_token = var.sysdig_secure_api_token
 }
 
 module "s4c_single_account_k8s_example" {
