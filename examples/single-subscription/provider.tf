89381723f7ce56b06af3a6c061668461d28422c5
regiluze <ruben.eguiluz@sysdig.com>
2022-07-09 02:06:47 +0200

    feat: support multiple registries for tenant subscriptions k8s

--- examples/single-subscription/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "bcd96c64-7593-472e-a7a4-42263462de7a"
-}
-
-provider "sysdig" {
-  sysdig_secure_url       = "https://secure.sysdig.com"
-  sysdig_secure_api_token = "d1c65fe1-fc21-4a86-b465-8347c971da9d"
-}
--- examples/tenant-subscriptions/scanning.tf
+++ examples/tenant-subscriptions/scanning.tf
@@ -1,6 +1,5 @@
 locals {
-  container_registry           = length(module.infrastructure_container_registry) > 0 ? module.infrastructure_container_registry[0].container_registry : ""
-  registry_resource_group_name = var.registry_resource_group_name == "" ? module.infrastructure_resource_group.resource_group_name : var.registry_resource_group_name
+  container_registry = length(module.infrastructure_container_registry) > 0 ? module.infrastructure_container_registry[0].container_registry : ""
 }
 
 
@@ -32,7 +31,7 @@ module "infrastructure_container_registry" {
 
   location             = var.location
   name                 = var.name
-  registry_name        = var.registry_name
-  resource_group_name  = local.registry_resource_group_name
+  existing_registries  = var.existing_registries
+  resource_group_name  = module.infrastructure_resource_group.resource_group_name
   eventhub_endpoint_id = local.eventgrid_eventhub_id
 }
